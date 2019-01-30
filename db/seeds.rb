6160f1f976cd8dd5cfadd9fa800844cc54e74ac8
apouille <nguon.melanie@gmail.com>
2019-01-30 19:52:53 +0100

    Ajout fichiers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+10.times do
+   course = Course.create!(title: Faker::Educator.course, content: Faker::Cannabis.cannabinoid_abbreviation)
+end
+
+10.times do 
+	lesson = Lesson.create!(title: Faker::Beer.name, content: Faker::FamousLastWords.last_words, course: Course.all.sample)
+end
